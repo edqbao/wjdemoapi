4b63d1b5540028997d38a35a7258d32b1728b237
Edward Bao <Ebao@MacBook-Pro.local>
2018-03-07 13:09:56 -0600

    commiting

--- app.js
+++ app.js
@@ -77,6 +77,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3008, function(){
+var server = app.listen( process.env.PORT || 3020, function(){
   console.log('Listening on port ' + server.address().port);
 });
